27a256b4dbfb840a2d859cbcbcbbbacf587bcd5a
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-04-13 16:29:16 -0300

    Implement vibrate

--- DemoVibrate/DemoVibrate/DemoVibrate/MainPage.xaml.cs
+++ DemoVibrate/DemoVibrate/DemoVibrate/MainPage.xaml.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Vibrate;
 using Xamarin.Forms;
 
 namespace DemoVibrate
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonClick(object sender, EventArgs e)
+        {
+            CrossVibrate.Current.Vibration(TimeSpan.FromSeconds(1));
+        }
+    }
 }
